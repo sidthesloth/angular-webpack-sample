e10f47fd89a01af5fde79b84d958b0a13d6cf08b
sidtheant <den.saka@gmail.com>
2016-08-09 11:30:10 +0200

    commit after initial changes

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,32 @@
+/**
+ * Created by sidtheant on 8/9/16.
+ */
+
+import 'bootstrap/dist/css/bootstrap.css';
 import angular from 'angular';
+import routing from './app.config';
 
 import '../style/app.css';
 
 let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
+    return {
+        template: require('./app.html'),
+        controller: 'AppCtrl',
+        controllerAs: 'app'
+    }
 };
 
 class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
+    constructor() {
+        this.url = 'https://github.com/preboot/angular-webpack';
+    }
 }
 
-const MODULE_NAME = 'app';
+const MODULE_NAME = 'atrsystem';
 
 angular.module(MODULE_NAME, [])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+    .config(routing)
+    .directive('app', app)
+    .controller('AppCtrl', AppCtrl);
 
 export default MODULE_NAME;
